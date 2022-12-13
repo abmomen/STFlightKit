54f8bfc044d4397848d1c7b38e3996bfaccbf6f3
Abdul Momen <abdulmomen130@gmail.com>
2022-12-13 09:46:44 +0600

    Initial commit

---
+++ Example/STFlightKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  STFlightKit
+//
+//  Created by 20514535 on 12/13/2022.
+//  Copyright (c) 2022 20514535. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
